8db694bb1e28b0c1493305ce7bc219a103a65c94
toru <toru@toru-Vostro-3558>
2017-11-09 15:04:47 +0900

    ややこしいのでクライアントサンプルを変更

--- heizu_server/client/player_client.rb
+++ heizu_server/client/player_client.rb
@@ -3,47 +3,39 @@ require 'json'
 
 Thread.abort_on_exception = true
 
-team_names = ['foo', 'goo']
+team_name = 'foo'
 
-threads = []
+# サーバ接続 OPEN
+sock = TCPSocket.open("localhost", 20000)
 
-team_names.each {|team_name|
-  threads << Thread.new(team_name) {|team_name|
-    # サーバ接続 OPEN
-    sock = TCPSocket.open("localhost", 20000)
+#チーム名要求
+puts sock.gets
 
-    #チーム名要求
-    puts sock.gets
+# チーム名の通知
+sock.puts JSON.generate({"team_name" => team_name})
+puts team_name
+sock.flush
 
-    # チーム名の通知
-    sock.puts JSON.generate({"team_name" => team_name})
-    puts team_name
+# ハンドシェイク
+puts "ハンドシェイク", sock.gets
+
+# 盤面情報
+loop do
+  puts board = JSON.parse(sock.gets)
+
+  break if(board["finished"])
+
+  # 自分のターンのとき
+  if(board["turn_team"] == team_name)
+    puts "my turn"
+    # 実際には行動JSONを送る
+    sock.puts '{"a": "b"}'
     sock.flush
 
-    # ハンドシェイク
-    puts "ハンドシェイク", sock.gets
+    # 結果を取得
+    puts result = JSON.parse(sock.gets)
+  end
+end
 
-    # 盤面情報
-    loop do
-      puts board = JSON.parse(sock.gets)
-
-      break if(board["finished"])
-
-      # 自分のターンのとき
-      if(board["turn_team"] == team_name)
-        puts "my turn"
-        # 実際には行動JSONを送る
-        sock.puts '{"a": "b"}'
-        sock.flush
-
-        # 結果を取得
-        puts result = JSON.parse(sock.gets)
-      end
-    end
-
-    # ソケット CLOSE
-    sock.close
-  }
-}
-
-threads.each{|t| t.join}
+# ソケット CLOSE
+sock.close
---
+++ heizu_server/client/player_client_test.rb
@@ -0,0 +1,53 @@
+require 'socket'
+require 'json'
+
+Thread.abort_on_exception = true
+
+=begin
+2台分のクライアントを立てる
+=end
+
+team_names = ['foo', 'goo']
+
+threads = []
+
+team_names.each {|team_name|
+  threads << Thread.new(team_name) {|team_name|
+    # サーバ接続 OPEN
+    sock = TCPSocket.open("localhost", 20000)
+
+    #チーム名要求
+    puts sock.gets
+
+    # チーム名の通知
+    sock.puts JSON.generate({"team_name" => team_name})
+    puts team_name
+    sock.flush
+
+    # ハンドシェイク
+    puts "ハンドシェイク", sock.gets
+
+    # 盤面情報
+    loop do
+      puts board = JSON.parse(sock.gets)
+
+      break if(board["finished"])
+
+      # 自分のターンのとき
+      if(board["turn_team"] == team_name)
+        puts "my turn"
+        # 実際には行動JSONを送る
+        sock.puts '{"a": "b"}'
+        sock.flush
+
+        # 結果を取得
+        puts result = JSON.parse(sock.gets)
+      end
+    end
+
+    # ソケット CLOSE
+    sock.close
+  }
+}
+
+threads.each{|t| t.join}
