43b2ce8e66352813e2111a91dd5f6a1ed6c5f444
toru <toru@toru-Vostro-3558>
2017-12-07 16:12:50 +0900

    コマンドライン引数

--- src/ruby/client/audience_client.rb
+++ src/ruby/client/audience_client.rb
@@ -1,7 +1,10 @@
 require 'socket'
 
+address = ARGV[0].nil? ? 'localhost' : ARGS[0]
+port = ARGV[1].nil? ? 20001 : ARGS[1].to_s
+
 # サーバ接続 OPEN
-sock = TCPSocket.open("localhost", 20001)
+sock = TCPSocket.open(address, port)
 
 loop do
   print board = sock.gets
